6c1e6f790ebc54c019c1a34be1284310e82b81ae
I750772 <ujjwal.tyagi@sap.com>
2025-10-06 20:57:24 +0530

    creating basic config

--- platform-api/src/config/config.module.ts
+++ platform-api/src/config/config.module.ts
@@ -1,1 +1,10 @@
 // empty
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+export class ConfigService {
+    readonly port = Number(process.env.PORT ?? 4000);
+    readonly databaseUrl = process.env.DATABASE_URL;
+    readonly redisUrl = process.env.REDIS_URL;
+    readonly shopifyWebhookSecret = process.env.SHOPIFY_WEBHOOK_SECRET!;
+}
--- platform-api/src/db/typeorm.config.ts
+++ platform-api/src/db/typeorm.config.ts
@@ -1,1 +1,28 @@
-// empty
+import { DataSource } from "typeorm";
+import { Merchant } from "../entities/merchant.entity";
+import { Product } from "../entities/product.entity";
+import { MerchantListing } from "../entities/merchant-listing.entity";
+import { Order } from "../entities/order.entity";
+import { OrderItem } from "../entities/order-item.entity";
+import { Shipment } from "../entities/shipment.entity";
+import { IdempotencyKey } from "../entities/idempotency-key.entity";
+import { Inventory } from "../entities/inventory.entity";
+import { ConfigService } from "../config/config.service";
+
+const cfg = new ConfigService();
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  url: cfg.databaseUrl,
+  ssl: false,
+  entities: [
+    Merchant,
+    Product,
+    MerchantListing,
+    Order,
+    OrderItem,
+    Shipment,
+    IdempotencyKey,
+    Inventory,
+  ],
+  synchronize: false,
+});
